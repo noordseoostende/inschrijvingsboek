2f9346ff1424f99618a38ef91c98abbd746796f3
noordseoostende <voropayev1@mail.ru>
2021-01-19 18:48:04 +0100

    de mogelijkheid een tekst toevoegen

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addLog } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
 
   const[message, setMessage] = useState('');
   const[attention, setAttention] = useState(false);
@@ -11,8 +14,16 @@ const AddLogModal = () => {
     if (message === '' || tech === '') {
       M.toast({ html: 'aub voer een boodschap en overzicht in' });
     } else {
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      }
+      addLog(newLog);
 
-      console.log(message, tech, attention);
+      M.toast({ html: `Log added by ${tech}`});
+      
       // Clear Filds
       setMessage('');
       setTech('');
@@ -84,8 +95,13 @@ const AddLogModal = () => {
 
   );
 };
+
+AddLogModal.propTypes = {
+  addLog: PropTypes.func.isRequired
+}
+
 const modalStyle = {
   width: '75%',
   height: '75%'
 };
-export default AddLogModal;
+export default connect(null, {addLog})(AddLogModal);
